a176b94d6e70182b0fcce8c57b3af5a7525e0bf9
Tanel.Ehatamm <tanel.ehatamm@ttu.ee>
2022-09-11 23:16:13 +0300

    ITB8201 - Fix instructions and logging

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const instructions = 'Send a GET request to "https://hello-itb8201.herokuapp.com//hello/name" where "name" is the' +
-    ' name you want to say hello to.';
+const instructionsHeader = '<h3>Instructions</h3>'
+const instructions = '<p>Send a GET request to "https://hello-itb8201.herokuapp.com/hello/name" where "name" is the' +
+    ' name you want to say hello to.</p>';
 
 app.get('/', (req, res) => {
-    res.send(instructions);
+    res.send(instructionsHeader + instructions);
 })
 
 app.get('/hello/:name', (req, res) => {
@@ -14,5 +15,5 @@ app.get('/hello/:name', (req, res) => {
 })
 
 app.listen(process.env.PORT || port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Hello app started!`)
 })
